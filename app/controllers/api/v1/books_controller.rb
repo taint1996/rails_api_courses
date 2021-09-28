6d644007ad6f8104168deaa413b0e4b4df66bbf4
Nguyen Trung Tai (TJ) <tai.nguyentrung1996@gmail.com>
2021-09-28 15:44:58 +0700

    Done API Index - Show Book

---
+++ app/controllers/api/v1/books_controller.rb
@@ -0,0 +1,22 @@
+class Api::V1::BooksController < ApplicationController
+  before_action :load_book, only: :show
+
+  def index
+    @books = Book.all.order(created_at: :desc)
+
+    json_response "Index Books", true, { books: @books}, :ok
+  end
+
+  def show
+    json_response "Show book successfully", true, { book: @book}, :ok
+  end
+
+  private
+    def load_book
+      @book = Book.find_by(id: params[:id])
+
+      unless @book.present?
+        json_response "Cannot get book", false, {}, :not_found
+      end
+    end
+end
